a2d4be5c49e66ea5c513d783bf1b4d0ac3cf6777
Paolo <71812497+paolomartini25@users.noreply.github.com>
2020-11-11 17:27:56 +0100

    Add files via upload

--- es1.0.0.c
+++ es1.0.0.c
@@ -5,28 +5,28 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool sommadispari(int n); 
+bool sommadispari(int n);  //funzione che somma i primi n numeri dispari
 
 int main(){
-    int n;
-    bool corretto;
+    int n; //numero inserito dall'utente
+    bool corretto; //se la somma dei primi n numeri dispari è uguale al quadrato di n = true
 
     printf("inserisci un numero maggiore di 0:\n");
     do{
-        scanf("%d", &n);
+        scanf("%d", &n); //inserimento di n
 
-        if (n<0){
+        if (n<0){//se il numero è minore di 0
             printf("Errore, il numero che hai inserito è minore di 0.\n");
         }
 
-    }while(n<0);
+    }while(n<0);//se il numero è maggiore d 0 si esce dal ciclo sennò si richiede il dato
 
     corretto = sommadispari(n);
 
-    if(corretto){
+    if(corretto){ //se la somma dei primi n numeri dispari è uguale al quadrato di n
         printf("La somma dei primi %d numeri dispari e' uguale al quadrato di %d\n", n, n);
     }
-    else{
+    else{ //se la somma dei primi n numeri dispari è diversa dal quadrato di n
         printf("La somma dei primi %d numeri dispari non e' uguale al quadrato di %d\n", n, n);
     }
 
@@ -35,14 +35,14 @@ int main(){
 
 bool sommadispari(int n){
     int somma = 0;
-    bool potenza = false;
+    bool potenza = false; //imposto l'uguaglianza'tra potenza e somma dei primi n numeri dispari a falsa
 
-    for (int i = 0; i < n; i++){
-        somma+=(i*2)+1;
+    for (int i = 0; i < n; i++){ //ripeto n volte
+        somma+=(i*2)+1; //(i*2)+1= numero disari
     }
     if (somma == n*n){
-        potenza = true;
+        potenza = true; //se è uguale imposto a vero
     }
 
-    return potenza;
+    return potenza; //ritorno il booleano dell'uguaglianza
 }
--- es1.1.0.c
+++ es1.1.0.c
@@ -4,55 +4,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int conversione(int bin, int cifra_attuale);
+int conversione(int bin, int cifra_attuale); //conversione da ciffra binaria a decimle
 
 int main(){
-    int bin, dec = 0;
-    int num_cifre;
+    int bin, dec = 0; //cifra binaria, numero decimale
+    int num_cifre;//numero di cifre del numero binario
 
     printf("inserisci il numero di cifre del numero binario che andrai ad inserire, deve essere maggiore di 0:\n");
     do{
-        scanf("%d", &num_cifre);
+        scanf("%d", &num_cifre); //inserimento numero di cifre
 
-        if (num_cifre<0){
+        if (num_cifre<0){ //se il numero è minore di 0
             printf("Errore, il numero che hai inserito è minore di 0.\n");
         }
 
-    }while(num_cifre<0);
+    }while(num_cifre<0); //se il numero è maggiore d 0 si esce dal ciclo sennò si richiede il dato
 
     printf("inserisci tutte le cifre del tuo numero binario partendo da quella meno significativa.\n111110 in questo caso lo 0 e' la cifra meno signifaciva quindi:\n0->1->1->[...]\n");
     
-    for (int j=0; j < num_cifre; j++){
+    for (int j=0; j < num_cifre; j++){ //ripete per quante sono le cifre
         printf("inserisci la cifra:\n");
         do{
-            scanf("%d", &bin);
+            scanf("%d", &bin);//inserimento della cifra
 
-            if (bin<0 || bin >1){
+            if (bin<0 || bin >1){ //se il numero è diverso da 0 o 1
                 printf("Errore, la cifra può essere solo 0 o 1.\n");
             }
 
-        }while(bin<0 || bin >1);
+        }while(bin<0 || bin >1); //se il numero è uguale a 0 o 1 si esce dal ciclo sennò si richiede il dato
 
-        int num =conversione(bin, j);
-        dec+=num;
+        int num =conversione(bin, j); //num contine la cifra convertita in decimale
+        dec+=num; //sommo tutte le cifre per ottenere il numero decimale
     }
 
-    printf("numero decimale = %d\n", dec);
+    printf("numero decimale = %d\n", dec); //stampo il numero convertito
     return 0;
 }
 
 int conversione(int bin, int cifra_attuale){
     
-    int num;
+    int num; //cifra convertita in decimale
 
     if (bin>0){
         for (int i=0; i<cifra_attuale; i++){ //num = bin*(2^cifra_attuale)
             num = bin * 2;
         } 
     }
-    else{
-        num = bin * 1;
+    else{ //cifra_attuale = 0
+        num = bin * 1; //2^0 = 1
     }
 
-    return num;
+    return num; //ritorno il numero
 }
